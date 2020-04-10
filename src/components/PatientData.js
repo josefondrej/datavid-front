849e6def3febba188e5b9ab53cce0391125ba5bf
Marek Polak <marek.polak@blindspot.ai>
2020-04-10 16:39:45 +0200

    Added form input fields.

--- src/components/PatientData.js
+++ src/components/PatientData.js
@@ -8,7 +8,7 @@ import Spinner from "react-bootstrap/Spinner";
 import contact from "./../util/contact.js";
 import { useForm } from "react-hook-form";
 
-function Contact(props) {
+function PatientData(props) {
   const [status, setStatus] = useState({ type: "none" });
   const { handleSubmit, register, errors, reset } = useForm();
 
@@ -33,51 +33,193 @@ function Contact(props) {
       <Form onSubmit={handleSubmit(onSubmit)}>
         <Form.Row>
           {props.showNameField && (
-            <Form.Group as={Col} xs={12} sm={6} controlId="formName">
+            <Form.Group as={Col} xs={12} sm={12} controlId="formAge">
               <FormField
                 size={props.inputSize}
-                name="name"
-                type="text"
-                placeholder="Name"
+                name="age"
+                type="number"
+                placeholder="Age"
                 error={errors.name}
                 inputRef={register({
-                  required: "Please enter your name"
+                  required: "Please enter age"
                 })}
               ></FormField>
             </Form.Group>
           )}
-
-          <Form.Group
-            as={Col}
-            xs={12}
-            sm={props.showNameField ? 6 : 12}
-            controlId="formEmail"
-          >
-            <FormField
-              size={props.inputSize}
-              name="email"
-              type="email"
-              placeholder="Email"
-              error={errors.email}
-              inputRef={register({
-                required: "Please enter your email"
-              })}
-            ></FormField>
-          </Form.Group>
         </Form.Row>
-        <Form.Group controlId="formMessage">
-          <FormField
-            size={props.inputSize}
-            name="message"
-            type="textarea"
-            placeholder="Message"
-            rows={5}
-            error={errors.message}
-            inputRef={register({
-              required: "Please enter a message"
-            })}
-          ></FormField>
-        </Form.Group>
+        <Form.Row>
+          {props.showNameField && (
+            <Form.Group as={Col} xs={12} sm={12} controlId="formSex">
+              <FormField
+                size={props.inputSize}
+                name="sex"
+                type="text"
+                placeholder="Sex"
+                error={errors.name}
+                inputRef={register({
+                  required: "Please enter your sex"
+                })}
+              ></FormField>
+            </Form.Group>
+          )}
+        </Form.Row>
+        <Form.Row>
+          {props.showNameField && (
+            <Form.Group as={Col} xs={12} sm={12} controlId="formSymptoms">
+              <FormField
+                size={props.inputSize}
+                name="symptoms"
+                type="text"
+                placeholder="Symptoms"
+                error={errors.name}
+                inputRef={register({
+                  required: "Please enter symptoms"
+                })}
+              ></FormField>
+            </Form.Group>
+          )}
+        </Form.Row>
+        <Form.Row>
+          {props.showNameField && (
+            <Form.Group as={Col} xs={12} sm={12} controlId="formDateOfFirstSymptoms">
+              <FormField
+                size={props.inputSize}
+                name="dateOfFirstSymptoms"
+                type="date"
+                placeholder="DateOfFirstSymptoms"
+                error={errors.name}
+                inputRef={register({
+                  required: "Please enter date of first Symptoms"
+                })}
+              ></FormField>
+            </Form.Group>
+          )}
+        </Form.Row>
+        <Form.Row>
+          {props.showNameField && (
+            <Form.Group as={Col} xs={12} sm={12} controlId="formComorbidity">
+              <FormField
+                size={props.inputSize}
+                name="comorbidity"
+                type="text"
+                placeholder="Comorbidity"
+                error={errors.name}
+                inputRef={register({
+                  required: "Please enter comorbidity"
+                })}
+              ></FormField>
+            </Form.Group>
+          )}
+        </Form.Row>
+        <Form.Row>
+          {props.showNameField && (
+            <Form.Group as={Col} xs={12} sm={12} controlId="formEpidemiologicalAnamnesis">
+              <FormField
+                size={props.inputSize}
+                name="epidemiologicalAnamnesis"
+                type="text"
+                placeholder="Epidemiological anamnesis"
+                error={errors.name}
+                inputRef={register({
+                  required: "Please enter epidemiological anamnesis"
+                })}
+              ></FormField>
+            </Form.Group>
+          )}
+        </Form.Row>
+        <Form.Row>
+          {props.showNameField && (
+            <Form.Group as={Col} xs={12} sm={12} controlId="formChronicMedication">
+              <FormField
+                size={props.inputSize}
+                name="chronicMedication"
+                type="text"
+                placeholder="Chronic medication"
+                error={errors.name}
+                inputRef={register({
+                  required: "Please enter chronic medication"
+                })}
+              ></FormField>
+            </Form.Group>
+          )}
+        </Form.Row>
+        <Form.Row>
+          {props.showNameField && (
+            <Form.Group as={Col} xs={12} sm={12} controlId="formConfirmationOfDisease">
+              <FormField
+                size={props.inputSize}
+                name="confirmationOfDisease"
+                type="text"
+                placeholder="Confirmation of disease"
+                error={errors.name}
+                inputRef={register({
+                  required: "Please enter confirmation of disease"
+                })}
+              ></FormField>
+            </Form.Group>
+          )}
+        </Form.Row>
+        <Form.Row>
+          {props.showNameField && (
+            <Form.Group as={Col} xs={12} sm={12} controlId="formComplicationsOfDisease">
+              <FormField
+                size={props.inputSize}
+                name="complicationsOfDisease"
+                type="text"
+                placeholder="Complications of disease"
+                error={errors.name}
+                inputRef={register({
+                  required: "Please enter complications of disease"
+                })}
+              ></FormField>
+            </Form.Group>
+          )}
+        </Form.Row>
+        <Form.Row>
+          {props.showNameField && (
+            <Form.Group as={Col} xs={12} sm={12} controlId="formTreatmentOfDisease">
+              <FormField
+                size={props.inputSize}
+                name="treatmentOfDisease"
+                type="text"
+                placeholder="Treatment of disease"
+                error={errors.name}
+                inputRef={register({
+                  required: "Please enter treatment of disease"
+                })}
+              ></FormField>
+            </Form.Group>
+          )}
+        </Form.Row>
+        <Form.Row>
+          {props.showNameField && (
+            <Form.Group as={Col} xs={12} sm={12} controlId="formOutcomeOfPatients">
+              <FormField
+                size={props.inputSize}
+                name="outcomeOfPatients"
+                type="text"
+                placeholder="Outcome of patients"
+                error={errors.name}
+                inputRef={register({
+                  required: "Please enter outcome of patients"
+                })}
+              ></FormField>
+            </Form.Group>
+          )}
+        </Form.Row>
+        {/*<Form.Group controlId="formMessage">*/}
+        {/*  <FormField*/}
+        {/*    size={props.inputSize}*/}
+        {/*    name="message"*/}
+        {/*    type="textarea"*/}
+        {/*    placeholder="Message"*/}
+        {/*    rows={5}*/}
+        {/*    error={errors.message}*/}
+        {/*    inputRef={register({*/}
+        {/*      required: "Please enter a message"*/}
+        {/*    })}*/}
+        {/*  ></FormField>*/}
+        {/*</Form.Group>*/}
         <Button
           variant={props.buttonColor}
           size={props.inputSize}
@@ -103,4 +245,4 @@ function Contact(props) {
   );
 }
 
-export default Contact;
+export default PatientData;
